6f06daf8da4c0af5893fc5b08707352cc7edbe94
Remi Boussu <remi.sylux@gmail.com>
2023-02-14 04:58:14 +0100

    init: added simple script to export bookmarklets from sources

---
+++ sources/sourcesToBookmarklet.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const { minify } = require("terser");
+
+const terserOptions = {
+    module: true
+};
+
+// https://gist.github.com/victorsollozzo/4134793 with some modifications
+function recursiveFindByExtension(base, files, result) {
+    files = files || fs.readdirSync(base)
+    result = result || [];
+
+    files.forEach(
+        function (file) {
+            var newbase = path.join(base, file)
+            if (fs.statSync(newbase).isDirectory()) {
+                result = recursiveFindByExtension(newbase, fs.readdirSync(newbase), result);
+            }
+            else {
+                result.push([newbase, file]);
+            }
+        }
+    )
+    return result;
+}
+
+async function main()
+{
+    let bookmarkletsPath = path.join(__dirname, "bookmarklets");
+    const filesToTreat = recursiveFindByExtension(bookmarkletsPath);
+
+    let bookmarklets = [];
+
+    await filesToTreat.forEach(async([filePath, fileName]) => {
+        let content = fs.readFileSync(filePath, "utf-8");
+        let compressedContent = (await minify(content, terserOptions)).code;
+        let bookmarkletContent = `javascript:(function(){${encodeURI(compressedContent)}})()`;
+        let htmlContent = `<p><a href=${bookmarkletContent}>${fileName}</a></p>`;
+        bookmarklets.push(htmlContent);
+    });
+
+    console.log("bookmarklets", bookmarklets)
+
+    let outputPath = path.resolve(bookmarkletsPath, "../../output/index.html");
+    let contentToWrite = bookmarklets.join("\n");
+    
+    let templatePath = path.join(__dirname, "template.html");
+    let templateContent = fs.readFileSync(templatePath, "utf-8");
+    let replacedContent = templateContent.replace("{BOOKMARKLETS}", contentToWrite);
+    
+    fs.writeFileSync(outputPath, replacedContent, () => {} );
+}
+
+main();
